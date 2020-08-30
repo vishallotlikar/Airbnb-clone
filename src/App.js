5504c6eee213647f57e4969601b4707a863a408e
vishal lotlikar <lotlikar18@gmail.com>
2020-08-30 15:11:32 +0530

    airbnb-clone WIP. Home components complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Header from './Header';
+import Footer from './Footer';
+import SearchPage from './SearchPage';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    // BEM
+    <div className="app">
+      {/* <h1>Hello</h1> */}
+      <Router>
+        <Header />
+
+        <Switch>
+          <Router path="/search">
+            <SearchPage />
+          </Router>
+          <Router path="/">
+            <Home />
+          </Router>
+        </Switch>
+
+        <Footer />
+      </Router>
+
+      {/* Home */}
+      {/* Header */}
+
+      {/* Banner */}
+      {/* Search */}
+
+      {/* Cards */}
+
+      {/* Footer */}
+
+      {/* SearchPage */}
+      {/*---*/}
     </div>
   );
 }
---
+++ src/SearchPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './SearchPage.css'
+
+function SearchPage() {
+    return (
+        <div className="serchPage">
+            <div className="searchPage__info">
+                <p>62 stays · 26 august to 30 august · 2 guest</p>
+                <h1>Stays nearby</h1>
+            </div>
+        </div>
+    )
+}
+
+export default SearchPage
